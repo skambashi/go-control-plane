aa8838443c810f149fd7b032049a8d1e218bd8d2
Alec Holmes <alecholmez@me.com>
2021-07-29 14:44:05 -0400

    generate artifacts from pprof
    Signed-off-by: Alec Holmes <alecholmez@me.com>

--- build/do_benchmarks.sh
+++ build/do_benchmarks.sh
@@ -5,6 +5,7 @@ set -x
 
 go install golang.org/x/tools/cmd/goimports@latest
 go get -u github.com/google/pprof
+apt update && apt install -y graphviz
 
 cd /go-control-plane
 
@@ -21,14 +22,29 @@ make benchmark MODE=3
 make benchmark MODE=4
 
 # generate our consumable pprof profiles
-# pprof -text bin/test goroutine_profile_xds.pb.gz > benchmarks/goroutine/goroutine_text.txt
-# pprof -tree bin/test goroutine_profile_xds.pb.gz > benchmarks/goroutine/goroutine_tree.txt
-# pprof -traces bin/test goroutine_profile_xds.pb.gz > benchmarks/goroutine/goroutine_trace.txt
+pprof -text bin/test cpu.pprof > benchmarks/reports/cpu_text.txt
+pprof -tree bin/test cpu.pprof > benchmarks/reports/cpu_tree.txt
+pprof -traces bin/test cpu.pprof > benchmarks/reports/cpu_trace.txt
 
-# pprof -text bin/test block_profile_xds.pb.gz > benchmarks/block/block_text.txt
-# pprof -tree bin/test block_profile_xds.pb.gz > benchmarks/block/block_tree.txt
-# pprof -traces bin/test block_profile_xds.pb.gz > benchmarks/block/block_trace.txt
+pprof -text bin/test goroutine.pprof > benchmarks/reports/goroutine_text.txt
+pprof -tree bin/test goroutine.pprof > benchmarks/reports/goroutine_tree.txt
+pprof -traces bin/test goroutine.pprof > benchmarks/reports/goroutine_trace.txt
 
-# pprof -text bin/test mutex_profile_xds.pb.gz > benchmarks/mutex/mutex_text.txt
-# pprof -tree bin/test mutex_profile_xds.pb.gz > benchmarks/mutex/mutex_tree.txt
-# pprof -traces bin/test mutex_profile_xds.pb.gz > benchmarks/mutex/mutex_trace.txt
+pprof -text bin/test block.pprof > benchmarks/reports/block_text.txt
+pprof -tree bin/test block.pprof > benchmarks/reports/block_tree.txt
+pprof -traces bin/test block.pprof > benchmarks/reports/block_trace.txt
+
+pprof -text bin/test mutex.pprof > benchmarks/reports/mutex_text.txt
+pprof -tree bin/test mutex.pprof > benchmarks/reports/mutex_tree.txt
+pprof -traces bin/test mutex.pprof > benchmarks/reports/mutex_trace.txt
+
+pprof -text bin/test mem.pprof > benchmarks/reports/mem_text.txt
+pprof -tree bin/test mem.pprof > benchmarks/reports/mem_tree.txt
+pprof -traces bin/test mem.pprof > benchmarks/reports/mem_trace.txt
+
+# generate some cool pprof graphs
+pprof -png bin/test cpu.pprof > benchmarks/pngs/cpu_graph.png
+pprof -png bin/test goroutine.pprof > benchmarks/pngs/goroutine_graph.png
+pprof -png bin/test block.pprof > benchmarks/pngs/block_graph.png
+pprof -png bin/test mutex.pprof > benchmarks/pngs/mutex_graph.png
+pprof -png bin/test mem.pprof > benchmarks/pngs/mem_graph.png
